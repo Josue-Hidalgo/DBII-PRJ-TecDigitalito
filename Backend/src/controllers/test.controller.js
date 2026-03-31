5a74bfa715727056a3fcbf90513440ee5017bfe6
Josue Hidalgo <josuehidalgos2511@gmail.com>
2026-03-31 19:03:10 +0000

    Backend update - Adding POST

--- Backend/src/controllers/test.controller.js
+++ Backend/src/controllers/test.controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model');
 
-// GET /api/test
+// GET <- /api/test
 exports.getTest = async (req, res) => {
   try {
     const users = await User.find();
@@ -13,5 +13,23 @@ exports.getTest = async (req, res) => {
     res.status(500).json({
       error: error.message,
     });
+  }
+};
+
+// POST <- /api/users
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    const newUser = new User({ name, email});
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message,
+    });
+    
   }
 };
--- Backend/src/routes/test.routes.js
+++ Backend/src/routes/test.routes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const testController = require('../controllers/test.controller');
 
-// GET /api/test
+// GET <- /api/test
 router.get('/test', testController.getTest);
+
+// POST <- /api/users
+router.post('/users', testController.createUser);
 
 module.exports = router;
