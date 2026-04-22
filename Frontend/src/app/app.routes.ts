ea6b2bf4757ffafa6cd2659f92ad4449546b0fd7
Josue Hidalgo <josuehidalgos2511@gmail.com>
2026-04-22 16:21:39 -0600

    Base de Frontend

--- Frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { UsersComponent } from './features/users/users';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: UsersComponent
-    }
-];
